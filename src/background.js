fb52b168de1a9a35ec5ad4ecf2ccebf7857b8fca
Stefan Dietz <dev@stefan-dietz.eu>
2014-05-13 21:40:18 +0200

    tidied up, completed magnifier with scope

--- src/background.js
+++ src/background.js
@@ -1,8 +1,5 @@
 chrome.browserAction.onClicked.addListener(function () {
     chrome.tabs.captureVisibleTab(null, {format: 'png'}, function (dataUrl) {
-        console.log('captured', dataUrl);
-
-
         chrome.tabs.executeScript(null, {code: 'window.screenRulerScreenshot = "' + dataUrl + '";'}, function () {
             chrome.tabs.executeScript(null, {file: 'src/content_script.js'});
         });
--- src/content_script.js
+++ src/content_script.js
@@ -1,4 +1,74 @@
-var addCanvas = function () {
+var MAGNIFIER_ZOOM = 16;
+var MAGNIFIER_RADIUS = 100;
+var MAGNIFIER_OFFSET = 20;
+
+function init () {
+    ctx_image.drawImage(img, 0, 0);
+
+    canvas_ruler.addEventListener('mousemove', function (evt) {
+        ctx_ruler.clearRect(0, 0, canvas_ruler.width, canvas_ruler.height);
+
+        var position = {x: evt.x, y: evt.y};
+
+        drawMagnifier(position);
+        drawCrosshairs(position);
+    });
+
+    document.addEventListener('keydown', function (evt) {
+        if(evt.keyCode == 27) {
+            exit();
+        }
+    });
+}
+
+function drawMagnifier (position) {
+    ctx_ruler.save();
+    ctx_ruler.beginPath();
+    ctx_ruler.arc(position.x + MAGNIFIER_RADIUS + MAGNIFIER_OFFSET,
+                  position.y + MAGNIFIER_RADIUS + MAGNIFIER_OFFSET,
+                  MAGNIFIER_RADIUS, 0, Math.PI * 2, true);
+    ctx_ruler.lineWidth = 8;
+    ctx_ruler.stroke();
+    ctx_ruler.closePath();
+    ctx_ruler.clip();
+
+    for(var x = 0; x < MAGNIFIER_RADIUS * 2 / MAGNIFIER_ZOOM; x++) {
+        for(var y = 0; y < MAGNIFIER_RADIUS * 2 / MAGNIFIER_ZOOM; y++) {
+            var pixel = ctx_image.getImageData(position.x + x - MAGNIFIER_RADIUS / MAGNIFIER_ZOOM,
+                                               position.y - MAGNIFIER_RADIUS / MAGNIFIER_ZOOM + y, 1, 1).data;
+
+            ctx_ruler.fillStyle = 'rgb(' + pixel[0] + ', ' + pixel[1] + ', ' + pixel[2] + ')';
+            ctx_ruler.fillRect(position.x + MAGNIFIER_OFFSET + x * MAGNIFIER_ZOOM - MAGNIFIER_ZOOM / 2,
+                               position.y + MAGNIFIER_OFFSET + y * MAGNIFIER_ZOOM - MAGNIFIER_ZOOM / 2,
+                               MAGNIFIER_ZOOM, MAGNIFIER_ZOOM);
+        }
+    }
+
+    //draw scope:
+    ctx_ruler.strokeStyle = '#000';
+    ctx_ruler.lineWidth = 2;
+    ctx_ruler.strokeRect(position.x + MAGNIFIER_RADIUS + MAGNIFIER_OFFSET - 5 - MAGNIFIER_ZOOM / 2,
+                         position.y + MAGNIFIER_RADIUS + MAGNIFIER_OFFSET - 5 - MAGNIFIER_ZOOM / 2,
+                         MAGNIFIER_ZOOM + ctx_ruler.lineWidth,
+                         MAGNIFIER_ZOOM + ctx_ruler.lineWidth);
+
+    ctx_ruler.restore();
+}
+
+function drawCrosshairs (position) {
+    drawLine(0, position.y - 0.5, canvas_ruler.width, position.y - 0.5); //horizontal
+    drawLine(position.x - 0.5, 0, position.x - 0.5, canvas_ruler.height); //verical
+}
+
+function drawLine (fromX, fromY, toX, toY) {
+    ctx_ruler.beginPath();
+    ctx_ruler.moveTo(fromX, fromY);
+    ctx_ruler.lineTo(toX, toY);
+    ctx_ruler.closePath();
+    ctx_ruler.stroke();
+}
+
+function addCanvas () {
     var canvas = document.createElement('canvas');
     canvas.style.position = 'fixed';
     canvas.style.zIndex = '999999999';
@@ -9,7 +79,13 @@ var addCanvas = function () {
     document.body.appendChild(canvas);
 
     return canvas;
-};
+}
+
+function exit () {
+    canvas_image.remove();
+    canvas_ruler.remove();
+}
+
 
 var canvas_image = addCanvas();
 var ctx_image = canvas_image.getContext('2d');
@@ -18,33 +94,5 @@ var canvas_ruler = addCanvas();
 var ctx_ruler = canvas_ruler.getContext('2d');
 
 var img = new Image();
-img.onload = function () {
-  ctx_image.drawImage(img, 0, 0);
-
-  canvas_ruler.addEventListener('mousemove', function (evt) {
-    canvas_ruler.width = canvas_ruler.width; //better performance than clearRect
-
-      //clip:
-      ctx_ruler.save();
-      ctx_ruler.beginPath();
-      ctx_ruler.arc(evt.x + 120, evt.y + 120, 100, 0, Math.PI * 2, true);
-      ctx_ruler.strokeStyle = 'red';
-      ctx_ruler.stroke();
-      ctx_ruler.closePath();
-      ctx_ruler.clip();
-
-      //draw magnifier
-      for(var x = 0; x < 20; x++) {
-          for(var y = 0; y < 20; y++) {
-              var pixel = ctx_image.getImageData(evt.x + x - 10, evt.y - 10 + y, 1, 1).data;
-
-              ctx_ruler.fillStyle = 'rgb(' + pixel[0] + ', ' + pixel[1] + ', ' + pixel[2] + ')';
-              ctx_ruler.fillRect(evt.x + 20 + x * 10, evt.y + 20 + y * 10, 10, 10);
-          }
-      }
-
-      ctx_ruler.restore();
-  });
-};
-
+img.onload = init;
 img.src = window.screenRulerScreenshot;
