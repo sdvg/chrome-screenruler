65d31690014bf2463ac4df78a72b39c7cfc5cbd1
Stefan Dietz <dev@stefan-dietz.eu>
2014-05-29 13:54:21 +0200

    implemented chrome shortcut ("command") to launch ruler

--- src/background.js
+++ src/background.js
@@ -1,7 +1,15 @@
-chrome.browserAction.onClicked.addListener(function () {
+var inject = function () {
     chrome.tabs.captureVisibleTab(null, {format: 'png'}, function (dataUrl) {
         chrome.tabs.executeScript(null, {code: 'window.screenRulerScreenshot = "' + dataUrl + '";'}, function () {
             chrome.tabs.executeScript(null, {file: 'src/content_script.js'});
         });
     });
+};
+
+chrome.browserAction.onClicked.addListener(inject);
+
+chrome.commands.onCommand.addListener(function(command) {
+    if(command === 'open-screenruler') {
+        inject();
+    }
 });
